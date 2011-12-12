9349b53dedb0dbe5a5e557e3e61474c0a5992d67
tShopshire <tyler@apigee.com>
2011-12-11 22:48:17 -0800

    Updated README with requirement, added option of supplying token as argument

--- pivotalMetric.sh
+++ pivotalMetric.sh
@@ -8,18 +8,23 @@
 # to my ugly code.
 # I use XMLStarlet heavily, which is a command line tool for XML manipulation.
 
+TOKEN_ARGS=1
 EXPECTED_ARGS=2
 METRIC_FILE=metrics.csv
 
-if [ $# -ne $EXPECTED_ARGS ]
+if [ $# == $TOKEN_ARGS ]
 then
-  echo "Usage is $0 <UserName> <password>"
+  TOKEN=$1
+elif [ $# == $EXPECTED_ARGS ]
+then
+  #Get the token used for the header in later requests.
+  TOKEN=$(curl -s -u $1:$2 -X GET https://www.pivotaltracker.com/services/v3/tokens/active | xmlstarlet sel -t -m "(/token)" -v "(guid)")
+else
+  echo "Usage is $0 <UserName> <password> or $0 <token>"
   exit 0
 fi
 
 touch $METRIC_FILE
-#Get the token used for the header in later requests.
-TOKEN=$(curl -s -u $1:$2 -X GET https://www.pivotaltracker.com/services/v3/tokens/active | xmlstarlet sel -t -m "(/token)" -v "(guid)")
 
 PROJECTXML=$(curl -s -H "X-TrackerToken: $TOKEN" -X GET https://www.pivotaltracker.com/services/v3/projects )
 #Get the names and id's of al the projects that are accessible to the user.
